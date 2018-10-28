c489c5f8afa1a0a257aa6e691ceacbabc9f78e13
Emily Vong <emily.vong25@gmail.com>
2018-10-28 15:19:55 +0000

    modified for easy deploy and updated readme

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -1,84 +1,104 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 class Button extends React.Component {
-    constructor(props){
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }   
-   
-    handleClick () {
-        this.props.onClickFunction(this.props.incrementValue);
-    }
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
 
-    render() {
-        return (
-            <button className="btn btn-info mx-3" type="button" onClick={this.handleClick}>
-                +{this.props.incrementValue}
-            </button>
-        );
-    }
+	handleClick() {
+		this.props.onClickFunction(this.props.incrementValue);
+	}
+
+	render() {
+		return (
+			<button
+				className="btn btn-info mx-3"
+				type="button"
+				onClick={this.handleClick}>
+				+{this.props.incrementValue}
+			</button>
+		);
+	}
 }
 
 Button.propTypes = {
-    onClickFunction: PropTypes.func,
-    incrementValue: PropTypes.number
+	onClickFunction: PropTypes.func,
+	incrementValue: PropTypes.number
 };
 
-var Result = function (props){
-    return (
-        <div className="my-3">
-            <h3>Total sum: {props.counter}</h3>
-        </div>
-    );
+var Result = function(props) {
+	return (
+		<div className="my-3">
+			<h3>Total sum: {props.counter}</h3>
+		</div>
+	);
 };
 
 Result.propTypes = {
-    counter: PropTypes.number
+	counter: PropTypes.number
 };
 
 export class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            counter: 0 
-        };
-        this.incrementCounter = this.incrementCounter.bind(this);
-        this.resetCount = this.resetCount.bind(this);
-    }
+	constructor() {
+		super();
+		this.state = {
+			counter: 0
+		};
+		this.incrementCounter = this.incrementCounter.bind(this);
+		this.resetCount = this.resetCount.bind(this);
+	}
 
-    incrementCounter (incrementValue) {
-        this.setState( {
-            counter: this.state.counter + incrementValue
-        });
-    }
-    
-    resetCount() {
-        this.setState({
-            counter: 0
-        });
-    }
+	incrementCounter(incrementValue) {
+		this.setState({
+			counter: this.state.counter + incrementValue
+		});
+	}
 
-    render() {
-        return (
-            <div className="container">{/*need to fix card layout*/}
-                <div className="row mt-5">
-                    <div className="col-6 mx-auto">
-                        <div className="row d-flex justify-content-center">
-                            <Button incrementValue={1} onClickFunction={this.incrementCounter} />
-                            <Button incrementValue={10} onClickFunction={this.incrementCounter} />
-                            <Button incrementValue={100} onClickFunction={this.incrementCounter} />
-                            <Button incrementValue={1000} onClickFunction={this.incrementCounter} />
-                        </div>
-                        <div className="row d-flex justify-content-center">
-                            <Result counter={this.state.counter} />
-                        </div>
-                        <div className="row d-flex justify-content-center">
-                            <button type="button" className="btn btn-warning" onClick={this.resetCount}>Reset</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+	resetCount() {
+		this.setState({
+			counter: 0
+		});
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div className="row mt-5">
+					<div className="col-6 mx-auto">
+						<div className="row d-flex justify-content-center">
+							<Button
+								incrementValue={1}
+								onClickFunction={this.incrementCounter}
+							/>
+							<Button
+								incrementValue={10}
+								onClickFunction={this.incrementCounter}
+							/>
+							<Button
+								incrementValue={100}
+								onClickFunction={this.incrementCounter}
+							/>
+							<Button
+								incrementValue={1000}
+								onClickFunction={this.incrementCounter}
+							/>
+						</div>
+						<div className="row d-flex justify-content-center">
+							<Result counter={this.state.counter} />
+						</div>
+						<div className="row d-flex justify-content-center">
+							<button
+								type="button"
+								className="btn btn-warning"
+								onClick={this.resetCount}>
+								Reset
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,19 @@
 //import react into the bundle
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import ReactDOM from "react-dom";
+//import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
-import 'bootstrap';
+import "bootstrap";
 
 //include your index.scss file into the bundle
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-import {App} from './component/App.jsx';
+import { App } from "./component/App.jsx";
 
-//import your own components
-//import Counter from './component/Counter.jsx';
-//import Button from './component/Counter.jsx';
-//import App from './component/Counter.jsx';
-//import Button from './component/Button.jsx';
-//import Result from './component/Button.jsx';
-
-//render your react application
 ReactDOM.render(
-    <div>
-        <App />
-    </div>,
-    document.querySelector('#app')
+	<div>
+		<App />
+	</div>,
+	document.querySelector("#app")
 );
-
